57302efb4d041268db87acf653dc8196b0e75729
Cyrill Frei <cfrei89@gmail.com>
2020-05-05 15:46:52 +0200

    addad initial models and added the new models to the admin functionality

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Dish, Category, Additional
+
 # Register your models here.
+admin.site.register(Dish)
+admin.site.register(Category)
+admin.site.register(Additional)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Dish(models.Model):
+    name = models.CharField(max_length=64)
+    max_allowed_extras = models.PositiveIntegerField()
+    small_prize = models.FloatField(null=True, blank=True)
+    standard_prize = models.FloatField(null=True, blank=True)
+    large_prize = models.FloatField(null=True, blank=True)
+    category = models.ManyToManyField(Category, blank=True, related_name='category_content')
+
+    def __str__(self):
+        return f"{self.name} from Category {self.category}"
+
+class Additional(models.Model):
+    name = models.CharField(max_length=64)
+    addable_to = models.ManyToManyField(Dish, blank=True, related_name='addables')
+
+    def __str__(self):
+        return f"{self.name}"
